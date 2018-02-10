20e1b59c0432175556f4e7a7a7b5f8c872e4fc31
Xan <35841390+Xaniux@users.noreply.github.com>
2018-02-10 15:48:02 +0100

    Create debrid1fichier.sh

---
+++ debrid1fichier.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+
+#Choose destination folder
+ 
+Destination=
+
+
+
+#Get total number of links
+nmbrtotal=$#
+
+
+#Count links yet to download
+multi=$#
+
+
+  
+
+while [ $multi -ne 0 ]
+
+do
+
+	url=$1
+
+
+	#Get file name
+ 
+	linenumbernom=` curl --silent $url | grep -ni "name" | cut -f1 -d: `
+
+
+	linenumbernom=$(($linenumbernom + 1))
+
+
+	nom=`curl --silent $url | sed -n "$linenumbernom p" | cut -d ">" -f 2 | cut -d "<" -f 1
+`
+
+	
+	#Try to get download link
+	dlink=`curl --silent -X POST $url | grep -n "ok btn-general btn-orange" | cut -d'"' -f2`
+
+
+	
+	#Wait if there is time before next download allowed
+	if [ $dlink=submit ]
+
+	then
+
+		attente=`curl --silent -X POST $url | grep -n "must wait" | cut -d" " -f5`
+
+		echo "$attente minutes to wait."
+
+		sleep $(($attente + 1))m
+
+        	dlink=`curl --silent -X POST $url | grep -n "ok btn-general btn-orange" | cut -d'"' -f2`
+
+	fi
+
+	
+echo "Start download of $nom."
+
+
+	curl $dlink > $Destination/$nom
+
+	
+
+
+	#Download done, shift variables
+	
+	multi=$(($multi-1))
+
+
+	echo "Download of $nom is down."
+
+        echo "$(($nmbrtotal-$multi))/$nmbrtotal downloads done. "
+
+
+	shift
+
+done
